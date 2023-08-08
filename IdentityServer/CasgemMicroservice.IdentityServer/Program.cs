45252f4e6b646a50ff81e91bf561533efde9b57c
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-08-08 17:19:45 +0300

    AppUser

--- IdentityServer/CasgemMicroservice.IdentityServer/Program.cs
+++ IdentityServer/CasgemMicroservice.IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using CasgemMicroservice.IdentityServer.Data;
+using CasgemMicroservice.IdentityServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -44,6 +48,18 @@ namespace CasgemMicroservice.IdentityServer
                 }
 
                 var host = CreateHostBuilder(args).Build();
+                using (var scope = host.Services.CreateScope())
+                {
+                    var serviceProvider = scope.ServiceProvider;
+                    var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+                    applicationDbContext.Database.Migrate();
+                    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                    if (!userManager.Users.Any())
+                    {
+                        userManager.CreateAsync(new ApplicationUser { UserName = "hardal", Email = "hardal@gmail.com", NameSurname="Yunus Emre Hardal", City="Ankara" }, "123456Aa*").Wait();
+                    }
+                }
 
                 if (seed)
                 {
