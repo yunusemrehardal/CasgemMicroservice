3b34a365c39526fe603b4802d6e373884b1078dc
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-08-08 13:54:47 +0300

    ProductService

--- Services/Catalog/CasgemMicroservice.Services.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/CasgemMicroservice.Services.Catalog/Services/CategoryServices/CategoryService.cs
@@ -1,33 +1,72 @@
-﻿using CasgemMicroservice.Services.Catalog.Dtos.CategoryDtos;
+﻿using AutoMapper;
+using CasgemMicroservice.Services.Catalog.Dtos.CategoryDtos;
+using CasgemMicroservice.Services.Catalog.Models;
+using CasgemMicroservice.Services.Catalog.Settings;
 using CasgemMicroservice.Shared.Dtos;
+using MongoDB.Driver;
 
 namespace CasgemMicroservice.Services.Catalog.Services.CategoryServices
 {
     public class CategoryService : ICategoryService
     {
-        public Task<Response<NoContent>> CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper, IDatabaseSettings _databaseSettings)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
         }
 
-        public Task<Response<NoContent>> DeleteCategoryAsync(string id)
+
+        public async Task<Response<NoContent>> CreateCategoryAsync(CreateCategoryDto createCategoryDto)
         {
-            throw new NotImplementedException();
+            var value = _mapper.Map<Category>(createCategoryDto);
+            await _categoryCollection.InsertOneAsync(value);
+            return Response<NoContent>.Success(204);
         }
 
-        public Task<Response<List<ResultCategoryDto>>> GetAllCategoryAsync()
+        public async Task<Response<NoContent>> DeleteCategoryAsync(string id)
         {
-            throw new NotImplementedException();
+            var value = await _categoryCollection.DeleteOneAsync(x => x.CategoryID == id);
+            return Response<NoContent>.Success(204);
         }
 
-        public Task<Response<ResultCategoryDto>> GetByIdCategoryAsync(string id)
+        public async Task<Response<List<ResultCategoryDto>>> GetAllCategoryAsync()
         {
-            throw new NotImplementedException();
+            var values = await _categoryCollection.Find(x => true).ToListAsync();
+            return Response<List<ResultCategoryDto>>.Success(_mapper.Map<List<ResultCategoryDto>>(values), 200);
         }
 
-        public Task<Response<NoContent>> UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        public async Task<Response<ResultCategoryDto>> GetByIdCategoryAsync(string id)
         {
-            throw new NotImplementedException();
+            var values = await _categoryCollection.Find<Category>(x => x.CategoryID == id).FirstOrDefaultAsync();
+            if (values == null)
+            {
+                return Response<ResultCategoryDto>.Fail("Kategori Bulunamadı", 404);
+            }
+            else
+            {
+                return Response<ResultCategoryDto>.Success(_mapper.Map<ResultCategoryDto>(values), 200);
+            }
+        }
+
+        public async Task<Response<NoContent>> UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+        {
+            var values = _mapper.Map<Category>(updateCategoryDto);
+            var result = await _categoryCollection.FindOneAndReplaceAsync(x => x.CategoryID == updateCategoryDto.CategoryID, values);
+
+            if (result == null)
+            {
+                return Response<NoContent>.Fail("Güncellenecek kategori bulunamadı!", 404);
+            }
+            else
+            {
+                return Response<NoContent>.Success(204);
+            }
         }
     }
 }
--- Services/Catalog/CasgemMicroservice.Services.Catalog/Services/ProductServices/ProductService.cs
+++ Services/Catalog/CasgemMicroservice.Services.Catalog/Services/ProductServices/ProductService.cs
@@ -1,33 +1,64 @@
-﻿using CasgemMicroservice.Services.Catalog.Dtos.ProductDtos;
+﻿using AutoMapper;
+using CasgemMicroservice.Services.Catalog.Dtos.ProductDtos;
+using CasgemMicroservice.Services.Catalog.Models;
+using CasgemMicroservice.Services.Catalog.Settings;
 using CasgemMicroservice.Shared.Dtos;
+using MongoDB.Driver;
 
 namespace CasgemMicroservice.Services.Catalog.Services.ProductServices
 {
     public class ProductService : IProductService
     {
-        public Task<Response<NoContent>> CreateProductAsync(CreateProductDto createProductDto)
+        private readonly IMongoCollection<Product> _productCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public ProductService(IMapper mapper, IDatabaseSettings _databaseSettings)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _productCollection = database.GetCollection<Product>(_databaseSettings.ProductCollectionName);
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
         }
 
-        public Task<Response<NoContent>> DeleteProductAsync(string id)
+        public async Task<Response<NoContent>> CreateProductAsync(CreateProductDto createProductDto)
         {
-            throw new NotImplementedException();
+            var values = _mapper.Map<Product>(createProductDto);
+            await _productCollection.InsertOneAsync(values);
+            return Response<NoContent>.Success(201);
         }
 
-        public Task<Response<List<ResultProductDto>>> GetAllProductAsync()
+        public async Task<Response<NoContent>> DeleteProductAsync(string id)
         {
-            throw new NotImplementedException();
+            var value = await _productCollection.DeleteOneAsync(x => x.ProductID == id);
+            return Response<NoContent>.Success(204);
         }
 
-        public Task<Response<ResultProductDto>> GetByIdProductAsync(string id)
+        public async Task<Response<List<ResultProductDto>>> GetAllProductAsync()
         {
-            throw new NotImplementedException();
+            var values = await _productCollection.Find(x => true).ToListAsync();
+            return Response<List<ResultProductDto>>.Success(_mapper.Map<List<ResultProductDto>>(values), 200);
         }
 
-        public Task<Response<NoContent>> UpdateProductAsync(UpdateProductDto updateProductDto)
+        public async Task<Response<ResultProductDto>> GetByIdProductAsync(string id)
         {
-            throw new NotImplementedException();
+            var values = await _productCollection.Find<Product>(x => x.ProductID == id).FirstOrDefaultAsync();
+            if (values == null)
+            {
+                return Response<ResultProductDto>.Fail("Ürün bulunamadı", 404);
+            }
+            else
+            {
+                return Response<ResultProductDto>.Success(_mapper.Map<ResultProductDto>(values), 200);
+            }
+        }
+
+        public async Task<Response<NoContent>> UpdateProductAsync(UpdateProductDto updateProductDto)
+        {
+            var values = _mapper.Map<Product>(updateProductDto);
+            var result = await _productCollection.FindOneAndReplaceAsync(x => x.ProductID == updateProductDto.ProductID, values);
+            return Response<NoContent>.Success(204);
         }
     }
 }
