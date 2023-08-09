003c4ab926de5a9db5f90220b74a177d3cb7faae
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-08-09 12:18:43 +0300

    GetToken addUser

---
+++ IdentityServer/CasgemMicroservice.IdentityServer/Controller/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using CasgemMicroservice.IdentityServer.Dtos;
+using CasgemMicroservice.IdentityServer.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace CasgemMicroservice.IdentityServer.Controller
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.Username,
+                Email = signUpDto.Email,
+                City = signUpDto.City,
+                NameSurname = signUpDto.NameSurname
+            };
+            await _userManager.CreateAsync(user, signUpDto.Password);
+            return NoContent();
+        }
+    }
+}
