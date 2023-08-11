92cf0a2ed6355057e831c1cb83411293886e36cd
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-08-11 16:51:56 +0300

    Onion Architecture - Order

---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Application/Mappings/OrderProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CasgemMicroservice.Services.Order.Core.Application.Dtos.OrderingDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Order.Core.Application.Mappings
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<ResultOrderDto, Domain.Entities.Ordering>().ReverseMap();
+            CreateMap<CreateOrderDto, Domain.Entities.Ordering>().ReverseMap();
+            CreateMap<UpdateOrderDto, Domain.Entities.Ordering>().ReverseMap();
+        }
+    }
+}
